3b0428a533ca7c8d59a71984a2453c97e4b3df29
em2046 <em2046@qq.com>
2019-07-16 22:49:19 +0800

    inferno

--- src/render.js
+++ src/render.js
@@ -373,8 +373,7 @@ function patchChildren(
               }
 
               if (moved) {
-                // const seq = lis(source)
-                const seq = [0, 1]
+                const seq = lis(source)
                 let j = seq.length - 1
                 for (let i = nextLeft; i >= 0; i--) {
                   if (source[i] === -1) {
@@ -494,3 +493,49 @@ function patchComponent(prevVNode, nextVNode, container) {
 }
 
 // endregion
+
+// https://en.wikipedia.org/wiki/Longest_increasing_subsequence
+function lis(arr) {
+  const p = arr.slice()
+  const result = [0]
+  let i
+  let j
+  let u
+  let v
+  let c
+  const len = arr.length
+  for (i = 0; i < len; i++) {
+    const arrI = arr[i]
+    if (arrI !== 0) {
+      j = result[result.length - 1]
+      if (arr[j] < arrI) {
+        p[i] = j
+        result.push(i)
+        continue
+      }
+      u = 0
+      v = result.length - 1
+      while (u < v) {
+        c = ((u + v) / 2) | 0
+        if (arr[result[c]] < arrI) {
+          u = c + 1
+        } else {
+          v = c
+        }
+      }
+      if (arrI < arr[result[u]]) {
+        if (u > 0) {
+          p[i] = result[u - 1]
+        }
+        result[u] = i
+      }
+    }
+  }
+  u = result.length
+  v = result[u - 1]
+  while (u-- > 0) {
+    result[u] = v
+    v = p[v]
+  }
+  return result
+}
